83296152fbe7dcf3de74a10f801c7f3ae5de40a8
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2014-10-27 16:44:13 -0700

    wop

--- stats.py
+++ stats.py
@@ -1,29 +1,23 @@
 from collections import Counter
 from matplotlib import pyplot as plt
-import snap
 
-graph_file = 'computed/graph.bin'
+def degree_dist(graph):
+    degdist = {
+        'user': Counter(),
+        'business': Counter()
+    }
 
-f = snap.TFIn(graph_file)
-graph = snap.TNEANet.Load(f)
+    for n in graph.Nodes():
+        node_id = n.GetId()
+        node_type = graph.GetStrAttrDatN(node_id, "type")
+        degdist[node_type].update([n.GetDeg()])
 
-degdist = {
-    'user': Counter(),
-    'business': Counter()
-}
+    for node_type, d in degdist.iteritems():
+        plt.plot(d.keys(), d.values())
 
-for n in graph.Nodes():
-    node_id = n.GetId()
-    node_type = graph.GetStrAttrDatN(node_id, "type")
-    degdist[node_type].update([n.GetDeg()])
-
-print("> Displaying dist")
-for node_type, d in degdist.iteritems():
-    plt.plot(d.keys(), d.values())
-
-plt.legend(['User reviews', 'Business reviews'])
-plt.xscale('log')
-plt.xlabel('log(degree)')
-plt.yscale('log')
-plt.ylabel('log(count)')
-plt.show()
+    plt.legend(['User reviews', 'Business reviews'])
+    plt.xscale('log')
+    plt.xlabel('log(degree)')
+    plt.yscale('log')
+    plt.ylabel('log(count)')
+    plt.show()
