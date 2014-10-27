10aea9c218658b84c24d0763cbc0bdbb5020bae5
umuhondo <clement.ntwari.nshuti@gmail.com>
2014-10-26 17:06:15 -0700

    Add joining dates for business and users

--- loader.py
+++ loader.py
@@ -1,11 +1,22 @@
 from generator import generateYelpReview
+from datetime import datetime
+
 import snap
+
+ATTR_NODE_TYPE = 'type'
+ATTR_JOINING_DATE = 'yelping_since'
+ATTR_OPENING_DATE = 'opening_date'
+DATE_FORMAT = '%Y-%m-%d'
 
 review_file = 'dataset/yelp_academic_dataset_review.json'
 graph_file = 'computed/graph.bin'
 
 graph = snap.TNEANet.New()
 graph.AddStrAttrN('type')
+# date when user started yelping
+graph.AddStrAttrN(ATTR_JOINING_DATE)
+# proxy for business creation date (date of earliest review)
+graph.AddStrAttrN(ATTR_OPENING_DATE)
 graph.AddStrAttrE('date')
 graph.Reserve(300000, 1200000)
 
@@ -19,6 +30,9 @@ for user_encid, business_encid, date in generateYelpReview(review_file):
         user_node_id = graph.AddNode()
         graph.AddStrAttrDatN(user_node_id, 'user', 'type')
         user_index_encid[user_encid] = user_node_id
+
+        # add time when user joined
+        graph.AddStrAttrDatN(user_node_id,date,ATTR_JOINING_DATE)
     else:
         user_node_id = user_index_encid[user_encid]
     
@@ -27,9 +41,19 @@ for user_encid, business_encid, date in generateYelpReview(review_file):
         business_node_id = graph.AddNode()
         graph.AddStrAttrDatN(business_node_id, 'business', 'type')
         business_index_encid[business_encid] = business_node_id
+        # temporarily add current review creation as date
+        graph.AddStrAttrDatN(business_node_id,date,ATTR_OPENING_DATE)
+
     else:
         business_node_id = business_index_encid[business_encid]
-
+        # update opening_date if review creation precedes current estimate
+        str_date = graph.GetStrAttrDatN(business_node_id,ATTR_OPENING_DATE)
+        new_estimate = datetime.strptime(date,DATE_FORMAT)
+        old_estimate = datetime.strptime(str_date,DATE_FORMAT)
+        if new_estimate < old_estimate:
+            new_date = new_estimate.isoformat().split("T")[0]
+            graph.AddStrAttrDatN(business_node_id,new_date,ATTR_OPENING_DATE)
+    
     edge = graph.AddEdge(user_node_id, business_node_id)
     graph.AddStrAttrDatE(edge, date, 'date')
 
