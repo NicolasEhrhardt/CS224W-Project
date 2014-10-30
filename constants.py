b6a5d093a6d5d215e90132c796f51e1e81d7f9d9
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2014-10-30 16:18:56 -0700

    wop

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Node attributes
+ATTR_NODE_TYPE = 'type'
+ATTR_NODE_CREATED_DATE = 'date'
+
+# The two types of node
+ATTR_NODE_BUSINESS_TYPE = 'business'
+ATTR_NODE_USER_TYPE = 'user'
+
+# Default create date for businesses (first review is used as proxy)
+DEFAULT_EARLY_DATE = '2000-01-01'
+
+# Edge attributes
+ATTR_EDGE_REVIEW_DATE = 'date'
+
+
