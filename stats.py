c325fa2ea0a6a5a8bf2d5d740abec0796900704b
umuhondo <clement.ntwari.nshuti@gmail.com>
2014-11-08 14:18:32 -0800

    Implement stats for densification,N(t),users over biz

--- stats.py
+++ stats.py
@@ -26,6 +26,109 @@ def degree_dist(graph):
     plt.ylabel('log(count)')
     plt.show()
 
+def densification_exponent(graph_file='computed/graph.bin',max_year=2014)
+
+  """ Plots the densification coefficient of the input graph. First it plot log-log plot\
+    of E(t) vs N(t). The it plots the time evolution of their ration """
+  
+  f = snap.TFIn(graph_file)
+  full_graph = snap.TNEANet.Load(f)
+
+  nodes,edges,users,busin = stats.nodes_and_edges_by_time(full_graph,max_year)
+
+  # Keep nodes and edges with count > 1
+  nodes = nodes[4::]
+  edges = edges[4::]
+
+  ## Compute densification exponent
+  idx_start = 0
+
+  plt.plot(nodes,edges,'ob-',linewidth=3.)
+  plt.yscale('log')
+  plt.xscale('log')
+  plt.xlabel('log N(t)')
+  plt.ylabel('log E(t)')
+  plt.grid(True)
+  plt.show()
+
+  # plot empirical densification exponent
+  dens_exp = [np.log(edges[idx])/np.log(nodes[idx]) for idx in range(len(nodes))]
+  plt.plot(dens_exp,'s--',linewidth=3.)
+  plt.xlabel('t')
+  plt.ylabel('log N(t) / log E(t)')
+  plt.grid(True)
+  plt.show()
+
+
+
+def nodes_evolution(graph_file='computed/graph.bin',max_year=2014)
+
+  """ Plots the densification coefficient of the input graph. First it plot log-log plot\
+    of E(t) vs N(t). The it plots the time evolution of their ration """
+  
+  f = snap.TFIn(graph_file)
+  full_graph = snap.TNEANet.Load(f)
+
+  nodes,edges,users,busin = stats.nodes_and_edges_by_time(full_graph,max_year)
+
+  # Keep nodes and edges with count > 1
+  nodes = nodes[4::]
+  edges = edges[4::]
+
+  ## Fit curve on N(t) = beta[0]*log(t) + beta[1]
+  time_start = len(nodes)/3
+  t = range(len(nodes))
+  time = t[time_start::]
+  nodes2 = nodes[time_start::]
+  onez = np.ones((len(time),1))
+
+  # solve linear regression
+  lgt = np.vstack((np.log(time),onez.transpose())).transpose()
+  lgy = np.log(nodes2)
+  XX = np.dot(lgt.transpose(),lgt)
+  Xb = np.dot(lgt.transpose(),lgy)
+  beta = np.linalg.solve(XX,Xb)
+
+  nodes_hat = np.multiply(np.power(time,beta[0]),np.exp(beta[1]))
+  plt.plot(nodes,'b-',linewidth=3.)
+  plt.plot(time,nodes_hat,'r--',linewidth=3.)
+  plt.yscale('log')
+  plt.grid(True)
+  plt.xlabel('Weeks')
+  plt.ylabel('Nodes')
+  beta1 = int(beta[1]*100)/100.
+  beta0 = int(beta[0]*100)/100.
+  curve = 'exp('+str(beta1) + ')*t^' + str(beta0)
+  plt.legend(('N(t)',curve))
+  plt.show()
+
+
+def users_vs_biz_evolution(graph_file='computed/graph.bin',max_year=2014)
+
+  """ Plots the densification coefficient of the input graph. First it plot log-log plot\
+    of E(t) vs N(t). The it plots the time evolution of their ration """
+  
+  f = snap.TFIn(graph_file)
+  full_graph = snap.TNEANet.Load(f)
+
+  nodes,edges,users,busin = stats.nodes_and_edges_by_time(full_graph,max_year)
+
+  # Keep nodes and edges with count > 1
+  nodes = nodes[4::]
+  edges = edges[4::]
+
+  ## users vs businesses
+  users = users[4::]
+  busin = busin[4::]
+  ratio_user_biz = [np.log(users[idx])/np.log(busin[idx]) for idx in range(len(users))]
+  plt.plot(ratio_user_biz,linewidth=3.)
+  plt.plot([0,len(ratio_user_biz)], [ratio_user_biz[-1], ratio_user_biz[-1]],'r--',linewidth=3.)
+  plt.xlabel('weeks')
+  plt.ylabel('log(nusers)/log(nbiz)')
+  plt.legend(('Observed','1.167'))
+  plt.show()
+
+
 def clustr_coeff_by_time(full_graph,dates=[]):
     "Dates is a sorted iterable of dates at the format YYYY-MM-DD. The function will incrementally \
      build a graph by adding edges and nodes according the dates in dates. For each date, the function \
@@ -43,7 +146,7 @@ def clustr_coeff_by_time(full_graph,dates=[]):
     plt.xticks(range(len(clustr_coeffs)),dates,size='small')
     plt.show()
 
-def nodes_and_edges_by_time(full_graph, max_year=2014):
+def nodes_and_edges_by_time(full_graph, max_year=2014,plots=False):
     "Dates is a sorted iterable of dates at the format YYYY-MM-DD. The function will incrementally \
      build a graph by adding edges and nodes according the dates in dates. For each date, the function \
      will store the number of nodes/edges created"
@@ -62,24 +165,25 @@ def nodes_and_edges_by_time(full_graph, max_year=2014):
     nodes_rate = [ nodes[i+1] - nodes[i] for i in range(len(nodes)-1) ]
     edges_rate = [ edges[i+1] - edges[i] for i in range(len(edges)-1) ]
 
-    plt.plot(nodes_rate,'b-')
-    plt.plot(edges_rate,'m--')
-    plt.legend(('nodes creation rate','edge creation rate'))
-    plt.show()
+    if plots:
+        plt.plot(nodes_rate,'b-')
+        plt.plot(edges_rate,'m--')
+        plt.legend(('nodes creation rate','edge creation rate'))
+        plt.show()
 
-    plt.plot(nodes,'b-')
-    plt.plot(edges,'m--')
-    plt.legend(('nodes','edges'))
-    plt.xlabel('month')
-    plt.ylabel('count')
-    plt.show()
+        plt.plot(nodes,'b-')
+        plt.plot(edges,'m--')
+        plt.legend(('nodes','edges'))
+        plt.xlabel('month')
+        plt.ylabel('count')
+        plt.show()
 
-    plt.plot(nusers,'b-')
-    plt.plot(nbusin,'m--')
-    plt.legend(('users','businesses'))
-    plt.xlabel('month')
-    plt.ylabel('count')
-    plt.show()
+        plt.plot(nusers,'b-')
+        plt.plot(nbusin,'m--')
+        plt.legend(('users','businesses'))
+        plt.xlabel('month')
+        plt.ylabel('count')
+        plt.show()
 
     return (nodes,edges,nusers,nbusin)
 
