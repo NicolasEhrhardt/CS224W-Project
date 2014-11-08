ebf05daa7369867218494262d45f6d0c1bea3bbd
umuhondo <clement.ntwari.nshuti@gmail.com>
2014-11-08 14:02:06 -0800

    Comment the analysis results

--- analysis/comments.md
+++ analysis/comments.md
@@ -2,4 +2,36 @@ Degree Distribution
 ===================
 
 ![Alt text](degdist.png "Degree Distribution")
+The degree distribution follows a power law distribution, typical of social networks
+
+Diameter Evolution
+==================
+
+![Alt text](diameter.png "Diameter evolution")
+The diameter of the graph quickly reaches a steady value of 12
+
+Nodes Evolution
+===============
+![Alt text](nodesApprox.png "Nodes arrival is superlinear")
+
+
+Densification Coefficient
+=========================
+
+![Alt text](densExp.png "Densification exponent")
+Contrary to what the theory tells us log(E(t))/log(N(t)) is not constant for this graph.
+One possible explanation is because there are several cities summarized in the graph and 
+they might have different node evolution rates dN(t)/dt and different densification
+exponents. 
+[TODO] Investigate this further.
+
+User vs business arrival rate
+=============================
+
+![Alt text](usersbiz.png "Users join the graph following a power of the number of businesses")
+Users join the graph at a higher rate than the businesses.
+
+From this we can get the probability that a given new node, belongs to the users of businesses sets.
+
+
 
--- test_init.py
+++ test_init.py
@@ -7,63 +7,73 @@ from projection import projection
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
 graph_file = 'computed/graph.bin'
 
 f = snap.TFIn(graph_file)
 full_graph = snap.TNEANet.Load(f)
 
-dates = []
+nodes,edges,users,busin = stats.nodes_and_edges_by_time(full_graph,2014)
 
-for year in range(2004,2014):
-    for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
-        date = str(year) + '-' + month + '-' + '01'
-        dates.append(date)
-
-dates = dates[10::]
-nodes,edges,users,busin = stats.nodes_and_edges_by_time(full_graph,dates)
-
-nodes = nodes[1::]
-edges = edges[1::]
-"""
+# Keep nodes and edges with count > 1
+nodes = nodes[4::]
+edges = edges[4::]
 
 ## Compute densification exponent
-lgedges = np.log(edges)
-lgnodes = np.log(nodes)
-onez = np.ones((len(edges),1))
-Xde = np.vstack(lgedges,onez.transpose()).transpose()
-XXde = np.dot(Xde.transpose(),Xde)
-Xbde = np.dot(Xde.transpose(),lgnodes)
-betade = np.linalg.solve(XXde,Xbde)
+idx_start = 0
 
-lgedges_hat = np.dot(Xde,betade.transpose())
-edges_hat = np.exp(lgedges_hat)
-plt.plot(nodes,edges,'b-',linewidth=3)
-plt.plot(nodes,edges_hat,'r--',linewidth=3)
+plt.plot(nodes,edges,'ob-',linewidth=3.)
 plt.yscale('log')
 plt.xscale('log')
+plt.xlabel('log N(t)')
+plt.ylabel('log E(t)')
 plt.grid(True)
 plt.show()
 
+# plot empirical densification exponent
+dens_exp = [np.log(edges[idx])/np.log(nodes[idx]) for idx in range(len(nodes))]
+plt.plot(dens_exp,'s--',linewidth=3.)
+plt.xlabel('t')
+plt.ylabel('log N(t) / log E(t)')
+plt.grid(True)
+plt.show()
+
+
 ## Fit curve on N(t)
-tpart2 = len(nodes)/2
+tpart2 = len(nodes)/3
 t = range(len(nodes))
 t2 = t[tpart2::]
 nodes2 = nodes[tpart2::]
 onez = np.ones((len(t2),1))
 
-lgt = np.vstack((np.log(t2),onez)).transpose()
+lgt = np.vstack((np.log(t2),onez.transpose())).transpose()
 lgy = np.log(nodes2)
 XX = np.dot(lgt.transpose(),lgt)
-Xb = np.dot(lgt.transpose(),nodes2)
+Xb = np.dot(lgt.transpose(),lgy)
 beta = np.linalg.solve(XX,Xb)
 
 nodes_hat = np.multiply(np.power(t2,beta[0]),np.exp(beta[1]))
 plt.plot(nodes,'b-',linewidth=3.)
 plt.plot(t2,nodes_hat,'r--',linewidth=3.)
+plt.yscale('log')
 plt.grid(True)
-plt.show()
 plt.xlabel('Weeks')
 plt.ylabel('Nodes')
+beta1 = int(beta[1]*100)/100.
+beta0 = int(beta[0]*100)/100.
+curve = 'exp('+str(beta1) + ')*t^' + str(beta0)
+plt.legend(('N(t)',curve))
+plt.show()
+
+## users vs businesses
+users = users[4::]
+busin = busin[4::]
+ratio_user_biz = [np.log(users[idx])/np.log(busin[idx]) for idx in range(len(users))]
+plt.plot(ratio_user_biz,linewidth=3.)
+plt.plot([0,len(ratio_user_biz)], [ratio_user_biz[-1], ratio_user_biz[-1]],'r--',linewidth=3.)
+plt.xlabel('weeks')
+plt.ylabel('log(nusers)/log(nbiz)')
+plt.legend(('Observed','1.167'))
+plt.show()
+
 
 
